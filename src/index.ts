aaaf062bb6e071a7ba51444146bd05c766c2a595
sonja-soennichsen <sonja.soennichsen@axelspringer.com>
2022-11-03 08:57:22 +0100

    seperate login and signup endpoints

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,13 @@ const cors = require("cors")
 const neo4j = require("neo4j-driver")
 require("dotenv").config()
 const { OGM } = require("@neo4j/graphql-ogm")
+const bodyParser = require("body-parser")
+const jsonParser = bodyParser.json()
 const cookieParser = require("cookie-parser")
 var jwt = require("jsonwebtoken")
 import resolvers from "./resolvers"
 const depthLimit = require("graphql-depth-limit")
+import { compare, hash, getSalt } from "./helpers/passwordUtils"
 
 const app = express()
 const corsOptions = {
@@ -65,34 +68,103 @@ export default Promise.all([neoSchema.getSchema(), ogm.init()]).then(
       schema,
       validationRules: [depthLimit(10)],
       context: async ({ res, req }: any) => {
-        if (req.url == "/login") {
-          //console.log("login")
-          return { req, res, User }
-        } else {
-          try {
-            const token = req.cookies["jwt"] || ""
-            const userJWT = jwt.verify(token, process.env.JWT_SECRET)
-            const [currentUser] = await User.find({
-              where: { id: userJWT.sub },
-            })
+        try {
+          const token = req.cookies["jwt"] || ""
+          const userJWT = jwt.verify(token, process.env.JWT_SECRET)
+          const [currentUser] = await User.find({
+            where: { id: userJWT.sub },
+          })
 
-            return {
-              req,
-              res,
-              User,
-              Resource,
-              Tag,
-              Comment,
-              Note,
-              currentUser,
-            }
-          } catch (e) {
-            throw new Error(e)
+          return {
+            req,
+            res,
+            User,
+            Resource,
+            Tag,
+            Comment,
+            Note,
+            currentUser,
           }
+        } catch (e) {
+          throw new Error(e)
         }
       },
       introspection: true,
       playground: true,
+    })
+
+    app.post("/login", jsonParser, async (req: any, res: any) => {
+      const password = req.body.password
+      const username = req.body.username
+
+      const user = await User.find({
+        where: { username: username },
+      })
+
+      if (!user) {
+        throw new Error(`User with username ${username} not found!`)
+      }
+
+      const correctPassword = compare(password, user[0].password, user[0].salt)
+
+      if (!correctPassword) {
+        throw new Error(
+          `Incorrect password for user with username ${username}!`
+        )
+      }
+
+      const token = jwt.sign(
+        { sub: user[0].id, username: username },
+        process.env.JWT_SECRET
+      )
+
+      res.cookie("jwt", token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+        sameSite: "none",
+        secure: true,
+      })
+
+      return res.json(token)
+    })
+
+    app.post("/signup", jsonParser, async (req: any, res: any) => {
+      const password = req.body.password
+      const username = req.body.username
+      const name = req.body.name
+      const email = req.body.email
+
+      const [existing] = await User.find({
+        where: {
+          username,
+        },
+      })
+
+      if (existing) {
+        throw new Error(`User with username ${username} already exists!`)
+      }
+
+      const salt = getSalt()
+      const hashedPassword = hash(password, salt)
+
+      const { users } = await User.create({
+        input: [
+          {
+            username,
+            password: hashedPassword,
+            salt,
+            name,
+            role: "User",
+            email,
+          },
+        ],
+      })
+      const token = jwt.sign(
+        { sub: users[0].id, username: username },
+        process.env.JWT_SECRET
+      )
+
+      return res.json(token)
     })
 
     await server.start()
@@ -101,6 +173,7 @@ export default Promise.all([neoSchema.getSchema(), ogm.init()]).then(
       path: "/graphql",
       cors: false,
     })
+
     app.use(express.urlencoded({ extended: true }))
 
     app.listen(4000, () => console.log(`🚀 Server ready at 4000`))

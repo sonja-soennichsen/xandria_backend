f3ecbc53c2b52a98c17c9276ba63fcd6f0c4af55
sonja-soennichsen <sonja.soennichsen@axelspringer.com>
2022-11-03 09:32:41 +0100

    refactor data structure

--- src/resolvers/mutations/index.ts
+++ src/resolvers/mutations/index.ts
@@ -1,5 +1,4 @@
 import resources from "./resources"
-import auth from "./auth"
 import user from "./user"
 import comment from "./comment"
 import note from "./note"
@@ -8,7 +7,6 @@ import tag from "./tag"
 export default {
   Mutation: {
     ...resources,
-    ...auth,
     ...user,
     ...comment,
     ...note,

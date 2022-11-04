5a7b21440425b8213f65271201b7938a31a8df1f
sonja-soennichsen <sonja.soennichsen@axelspringer.com>
2022-11-04 13:22:34 +0100

    I don't know what i'm doing

--- tests/__tests__/login.ts
+++ tests/__tests__/login.ts
@@ -1,37 +1,46 @@
-import request from "graphql-request"
-// https://www.npmjs.com/package/graphql-request
+// import request from "graphql-request"
+// // https://www.npmjs.com/package/graphql-request
+// const axios = require("axios")
 
-let header: {}
-it("logs in", async () => {
-  request({
-    url: "http://localhost:4000/graphql/login",
-    document: `mutation Mutation($username: String!, $password: String!) {
-      signIn(username: $username, password: $password)
-    }`,
-    variables: {
-      username: "username111",
-      password: "null",
-    },
-  }).then((data) => {
-    header = {
-      authorization: `Bearer ${data.signIn}`,
-    }
-    expect(data.signIn).toMatch("ey")
-  })
-})
+// const data = {
+//   username: "tesUser",
+//   password: "Ne1nfeefgew",
+//   name: "null",
+//   email: "null",
+// }
 
-it("requests resource with token", async () => {
-  request({
-    url: "http://localhost:4000/graphql",
-    document: `query Query {
-      resources {
-        headline
-        description
-        url
-      }
-    }`,
-    requestHeaders: header,
-  }).then((data) => {
-    expect(data).toBeTruthy
-  })
-})
+// const login = {
+//   username: "tesUser",
+//   password: "Ne1nfeefgew",
+// }
+
+// let header: {}
+// it("signs up", async () => {
+//    await axios.post("http://localhost:4000/signup", data).then((res: any) => {
+//     console.log(`Status: ${res.status}`)
+//     expect(res.status).toBe(200)
+//   })
+// })
+
+// it("logs in", async () => {
+//   await axios.post("http://localhost:4000/login", login).then((res: any) => {
+//     console.log(`Status: ${res.status}`)
+//     expect(res.status).toBe(200)
+//   })
+// })
+
+// it("requests resource with token", async () => {
+//   request({
+//     url: "http://localhost:4000/graphql",
+//     document: `query Query {
+//       resources {
+//         headline
+//         description
+//         url
+//       }
+//     }`,
+//     requestHeaders: header,
+//   }).then((data) => {
+//     expect(data).toBeTruthy
+//   })
+// })

8b419b646aa681b01beb62892c449f60ee14ead3
sonja-soennichsen <sonja.soennichsen@axelspringer.com>
2022-11-04 13:45:15 +0100

    fix cookie

--- src/auth/login.ts
+++ src/auth/login.ts
@@ -38,6 +38,7 @@ router.post("/", jsonParser, async (req: any, res: any) => {
     maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
     path: "/",
     sameSite: "none",
+    secure: true,
   })
 
   return res.status(200).json("succesfully logged in")

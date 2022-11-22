4370f0120e1bd963c92ffe9ff4199acb0d931694
sonja-soennichsen <sonja.soennichsen@axelspringer.com>
2022-11-22 18:40:54 +0100

    Add logger functionn

--- src/utils/logger_helper.ts
+++ src/utils/logger_helper.ts
@@ -6,8 +6,17 @@ export function create_logger() {
     format: combine(timestamp(), format.json(), prettyPrint()),
     transports: [
       new transports.File({ filename: "error.log", level: "error" }),
+      new transports.File({ filename: "info.log", level: "info" }),
       new transports.Console({ format: format.simple() }),
     ],
   }
   return new createLogger(myWinstonOptions)
+}
+
+export function getDurationInMilliseconds(start: [number, number]) {
+  const NS_PER_SEC = 1e9
+  const NS_TO_MS = 1e6
+  const diff = process.hrtime(start)
+
+  return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
 }

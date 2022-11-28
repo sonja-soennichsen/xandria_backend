4339e2684d809b3f4b759b13864d85e88c643da5
sonja-soennichsen <sonja.soennichsen@axelspringer.com>
2022-11-28 14:53:24 +0100

    Refactor: Extract context function

--- src/config/server_config.ts
+++ src/config/server_config.ts
@@ -1,34 +1,13 @@
 const depthLimit = require("graphql-depth-limit")
 const { ApolloServer } = require("apollo-server-express")
-var jwt = require("jsonwebtoken")
-import { check_context_auth } from "../utils/check"
-import { format_error } from "../utils/error_utils"
+import { create_context, format_error } from "../utils/server_utils"
 const { createComplexityLimitRule } = require("graphql-validation-complexity")
-import { User } from "../index"
 
 export const initialize_server = (schema: any) => {
   return new ApolloServer({
     schema,
     validationRules: [depthLimit(10), createComplexityLimitRule(2000)],
-    context: async ({ res, req }: any) => {
-      try {
-        const token = req.cookies["jwt"] || "" || req.headers["jwt"]
-        const userJWT = jwt.verify(token, process.env.JWT_SECRET)
-        const [currentUser] = await User.find({
-          where: { id: userJWT.sub },
-        })
-
-        check_context_auth(currentUser)
-
-        return {
-          req,
-          res,
-          currentUser,
-        }
-      } catch (e) {
-        throw new Error(e)
-      }
-    },
+    context: create_context,
     introspection: true,
     playground: true,
     formatError: format_error,
---
+++ src/utils/server_utils.ts
@@ -0,0 +1,36 @@
+import { ApolloError } from "apollo-server-express"
+import { User } from "../index"
+import { check_context_auth } from "../utils/check"
+var jwt = require("jsonwebtoken")
+
+export const format_error = (err: ApolloError) => {
+  if (err.message.startsWith("Expected")) {
+    return new Error("Internal server error -  Malformed Database Source")
+  }
+  if (err.message.startsWith("Context creation failed: JsonWebTokenError")) {
+    return new Error(
+      "Context creation failed: JsonWebTokenError: jwt must be provided"
+    )
+  }
+  return err
+}
+
+export const create_context = async ({ res, req }: any) => {
+  try {
+    const token = req.cookies["jwt"] || "" || req.headers["jwt"]
+    const userJWT = jwt.verify(token, process.env.JWT_SECRET)
+    const [currentUser] = await User.find({
+      where: { id: userJWT.sub },
+    })
+
+    check_context_auth(currentUser)
+
+    return {
+      req,
+      res,
+      currentUser,
+    }
+  } catch (e) {
+    throw new Error(e)
+  }
+}

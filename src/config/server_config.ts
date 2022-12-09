7bfd2cd5ca34386e7d9af1f0a0d3e8298d2def35
sonja-soennichsen <sonja.soennichsen@axelspringer.com>
2022-12-09 13:38:04 +0100

    restructure

--- src/config/server_config.ts
+++ src/config/server_config.ts
@@ -1,13 +1,34 @@
 const depthLimit = require("graphql-depth-limit")
 const { ApolloServer } = require("apollo-server-express")
-import { create_context, format_error } from "../utils/server_utils"
 const { createComplexityLimitRule } = require("graphql-validation-complexity")
+import { User } from "../index"
+import { check_context_auth } from "../utils/check"
+var jwt = require("jsonwebtoken")
+import { format_error } from "../utils/server_utils"
 
 export const initialize_server = (schema: any) => {
   return new ApolloServer({
     schema,
     validationRules: [depthLimit(10), createComplexityLimitRule(2000)],
-    context: create_context,
+    context: async ({ res, req }: any) => {
+      try {
+        const token = req.cookies["jwt"] || "" || req.headers["jwt"]
+        const userJWT = jwt.verify(token, process.env.JWT_SECRET)
+        const [currentUser] = await User.find({
+          where: { id: userJWT.sub },
+        })
+
+        check_context_auth(currentUser)
+
+        return {
+          req,
+          res,
+          currentUser,
+        }
+      } catch (e) {
+        throw new Error(e)
+      }
+    },
     introspection: true,
     playground: true,
     formatError: format_error,
--- src/utils/server_utils.ts
+++ src/utils/server_utils.ts
@@ -1,8 +1,4 @@
 import { ApolloError } from "apollo-server-express"
-import { User } from "../index"
-import { check_context_auth } from "../utils/check"
-var jwt = require("jsonwebtoken")
-import { UserType, JWT } from "../config/static"
 
 export const format_error = (err: ApolloError) => {
   if (err.message.startsWith("Expected")) {
@@ -14,22 +10,4 @@ export const format_error = (err: ApolloError) => {
     )
   }
   return err
-}
-
-export const create_context = async ({ res, req }: any) => {
-  try {
-    const token: string = req.cookies["jwt"] || "" || req.headers["jwt"]
-    const userJWT: JWT = jwt.verify(token, process.env.JWT_SECRET)
-    const [currentUser]: UserType[] = await User.find_by_id(userJWT.sub)
-
-    check_context_auth(currentUser)
-
-    return {
-      req,
-      res,
-      currentUser,
-    }
-  } catch (e) {
-    throw new Error(e)
-  }
 }
